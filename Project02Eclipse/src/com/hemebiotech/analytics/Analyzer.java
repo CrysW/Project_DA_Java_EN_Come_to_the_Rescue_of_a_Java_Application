74e03ed91b08c33ae67c773db0c48a6857f531e8
CrysW <dazas.sebastien@gmail.com>
2021-03-15 11:07:35 +0100

    Création de la fonction comptage de symptomes

--- Project02Eclipse/src/com/hemebiotech/analytics/Analyzer.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Analyzer.java
@@ -1,15 +1,18 @@
 package com.hemebiotech.analytics;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Read the symptoms from a file
+ * Count the symptoms
  */
 
 public class Analyzer {
 
     // Attribut(s)
     private ISymptomReader reader = new ReadSymptomDataFromFile();
+    private ISymptomCounter counter = new SymptomCounter();
 
     // Construteur(s)
     public Analyzer() {
@@ -20,11 +23,14 @@ public class Analyzer {
 
     /**
      * Read the symptoms
+     * Count the symptoms
      */
 
     public void analyse() {
 
         List<String> r = reader.getSymptoms();
+
+        Map<String,Integer> c = counter.counterList(r);
 
     }
 }
